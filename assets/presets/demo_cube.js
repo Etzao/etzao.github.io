2e8d8c77762293282f625837cef7cff6d42ab4e0
hanetzao@gmail.com <hanetzao@gmail.com>
2020-06-06 13:25:09 +0400

    fix

--- assets/presets/demo_cube.js
+++ assets/presets/demo_cube.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 // SETTINGS of this demo:
 const SETTINGS = {
   rotationOffsetX: 0, // negative -> look upper. in radians
@@ -11,8 +12,15 @@ const SETTINGS = {
 };
 
 // some globalz:
-let THREEVIDEOTEXTURE = null, THREERENDERER = null, THREEFACEOBJ3D = null, THREEFACEOBJ3DPIVOTED = null, THREESCENE = null, THREECAMERA = null, AFRAMEINSTANCE = null;
-let ISDETECTED = false, ISLOADED = false;
+let BABYLONVIDEOTEXTURE = null, BABYLONENGINE = null, BABYLONFACEOBJ3D = null, BABYLONFACEOBJ3DPIVOTED = null, BABYLONSCENE = null, BABYLONCAMERA = null, ASPECTRATIO = -1, JAWMESH = null;
+let ISDETECTED = false;
+
+
+// analoguous to GLSL smoothStep function:
+function smoothStep(edge0, edge1, x){
+  const t = Math.min(Math.max((x - edge0) / (edge1 - edge0), 0.0), 1.0);
+  return t * t * (3.0 - 2.0 * t);
+}
 
 // callback launched if a face is detected or lost:
 function detect_callback(isDetected){
@@ -23,108 +31,82 @@ function detect_callback(isDetected){
   }
 }
 
-// recursive function to find a specific element in the AFRAME sceneGraph:
-function extract_threeChildrenWithId(id, threeElt){
-  if (typeof(threeElt)==='undefined') return false;
-  if (!threeElt || !threeElt.type) return false;
-  if (threeElt.el && threeElt.el.id===id) return [threeElt];
-  if (!threeElt.children || threeElt.children.length===0) return false;
-  let r = [];
-  threeElt.children.forEach(function(threeChild){
-    const zou = extract_threeChildrenWithId(id, threeChild);
-    if (zou && zou.length){
-      r = r.concat(zou);
-    }
-  });
-  return r;
-}
+// build the 3D. called once when Jeeliz Face Filter is OK:
+function init_babylonScene(spec){
+  // INIT THE BABYLON.JS context:
+  BABYLONENGINE = new BABYLON.Engine(spec.GL);
 
-
-// Modified version of Aframe. called once when Jeeliz Face Filter is OK:
-function init_aFrame(spec){
-  AFRAMEINSTANCE = startAframe({
-    context: spec.GL,
-    canvas: spec.canvasElement
-  });
-
-  // get AFRAME initialized THREE.JS renderer:
-  THREERENDERER = AFRAMEINSTANCE.renderer;
-  init_threeScene(spec);
-}
-
-// build the 3D:
-function init_threeScene(spec){
-  // COMPOSITE OBJECT WHICH WILL FOLLOW THE HEAD
+  // CREATE THE SCENE:
+  BABYLONSCENE = new BABYLON.Scene(BABYLONENGINE);
+   
+  // COMPOSITE OBJECT WHICH WILL FOLLOW THE HEAD:
   // in fact we create 2 objects to be able to shift the pivot point
-  THREEFACEOBJ3D = new THREE.Object3D();
-  THREEFACEOBJ3D.frustumCulled = false;
-  THREEFACEOBJ3DPIVOTED = new THREE.Object3D();
-  THREEFACEOBJ3DPIVOTED.frustumCulled = false;
-  THREEFACEOBJ3DPIVOTED.position.set(0, -SETTINGS.pivotOffsetYZ[0], -SETTINGS.pivotOffsetYZ[1]);
-  THREEFACEOBJ3DPIVOTED.scale.set(SETTINGS.scale, SETTINGS.scale, SETTINGS.scale);
-  THREEFACEOBJ3D.add(THREEFACEOBJ3DPIVOTED);
+  BABYLONFACEOBJ3D = new BABYLON.Mesh();  
+  BABYLONFACEOBJ3DPIVOTED = new BABYLON.Mesh();
+  BABYLONFACEOBJ3DPIVOTED.position.set(0, -SETTINGS.pivotOffsetYZ[0], -SETTINGS.pivotOffsetYZ[1]);
+  BABYLONFACEOBJ3DPIVOTED.scaling.set(SETTINGS.scale, SETTINGS.scale, SETTINGS.scale);
+  BABYLONFACEOBJ3D.addChild(BABYLONFACEOBJ3DPIVOTED);
+  BABYLONSCENE.addMesh(BABYLONFACEOBJ3D);
 
-  // extract the scene from A-Frame:
-  THREESCENE = AFRAMEINSTANCE.object3D;
-  THREESCENE.add(THREEFACEOBJ3D);
+  // CREATE A CUBE:
+  const cubeMaterial = new BABYLON.StandardMaterial("material", BABYLONSCENE);
+  cubeMaterial.emissiveColor = new BABYLON.Color3(0, 0.28, 0.36);
+  const babylonCube = new BABYLON.Mesh.CreateBox("box", 1, BABYLONSCENE);
+  babylonCube.material = cubeMaterial;
+  BABYLONFACEOBJ3DPIVOTED.addChild(babylonCube);
+  babylonCube.position.set(0,0,0);
 
-  const threeFaceFollowers = extract_threeChildrenWithId('jeelizFaceFilterFollow', THREESCENE);
-  if (!threeFaceFollowers || !threeFaceFollowers.length){
-    alert('No element with id = jeelizFaceFilterFollow has been found in the A-Frame scene. You should have at least one. Otherwise none of your stuffs will follow the head');
-  } else  {
-    threeFaceFollowers.forEach(function(threeStuff){
-      THREEFACEOBJ3DPIVOTED.add(threeStuff);
-    })
-  }
-
-  // init video texture with red color:
-  THREEVIDEOTEXTURE = new THREE.DataTexture( new Uint8Array([255,0,0]), 1, 1, THREE.RGBFormat);
-  THREEVIDEOTEXTURE.needsUpdate = true;
-
-  // CREATE THE VIDEO BACKGROUND:
-  const videoMaterial = new THREE.RawShaderMaterial({
-    depthWrite: false,
-    vertexShader: "attribute vec2 position;\n\
-      varying vec2 vUV;\n\
-      void main(void){\n\
-        gl_Position=vec4(position, 1., 1.);\n\
-        vUV=0.5+0.5*position;\n\
-      }",
-    fragmentShader: "precision lowp float;\n\
-      uniform sampler2D samplerVideo;\n\
-      varying vec2 vUV;\n\
-      void main(void){\n\
-        gl_FragColor=texture2D(samplerVideo, vUV);\n\
-      }",
-     uniforms:{
-      samplerVideo: {value: THREEVIDEOTEXTURE}
-     }
-  });
-  const videoGeometry = new THREE.BufferGeometry()
-  const videoScreenCorners = new Float32Array([-1,-1,   1,-1,   1,1,   -1,1]);
-  videoGeometry.addAttribute( 'position', new THREE.BufferAttribute( videoScreenCorners, 2 ) );
-  videoGeometry.setIndex(new THREE.BufferAttribute(new Uint16Array([0,1,2, 0,2,3]), 1));
+  // CREATE THE MESH MOVING WITH THE JAW (mouth opening):
+  JAWMESH = BABYLON.MeshBuilder.CreateBox("jaw", {height: 0.3, width: 1, depth: 1}, BABYLONSCENE);
+  JAWMESH.material = cubeMaterial;
+  BABYLONFACEOBJ3DPIVOTED.addChild(JAWMESH);
+  JAWMESH.position.set(0,-(0.5+0.15+0.01),0);
   
-  const videoMesh = new THREE.Mesh(videoGeometry, videoMaterial);
-  videoMesh.onAfterRender=function(){
-    //replace THREEVIDEOTEXTURE.__webglTexture by the real video texture
-    THREERENDERER.properties.update(THREEVIDEOTEXTURE, '__webglTexture', spec.videoTexture);
-    delete(videoMesh.onAfterRender);
-  };
-  videoMesh.frustumCulled = false;
-  THREESCENE.add(videoMesh);
+  // ADD A LIGHT:
+  const pointLight = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(0, 1, 0), BABYLONSCENE);
+  pointLight.intensity = 0.5;
 
+  // init the video texture:
+  BABYLONVIDEOTEXTURE = new BABYLON.RawTexture(new Uint8Array([255,0,0,0]),1,1,spec.GL.RGBA,BABYLONSCENE);
+  BABYLONVIDEOTEXTURE._texture._webGLTexture = spec.videoTexture;
+  
+  // CREATE THE VIDEO BACKGROUND
+  // for custom material see https://gamedevelopment.tutsplus.com/tutorials/building-shaders-with-babylonjs-and-webgl-theory-and-examples--cms-24146
+  const videoMaterial = new BABYLON.ShaderMaterial(
+    'videoMat',
+    BABYLONSCENE,
+    {
+      vertexElement: "videoMatVertexShaderCode", //cf index.html for shader source
+      fragmentElement: "videoMatFragmentShaderCode"
+    },
+    {
+      attributes: ["position"],
+      uniforms: []
+      ,needAlphaBlending: false
+    }
+  );
+  videoMaterial.disableDepthWrite = true;
+  videoMaterial.setTexture("samplerVideo", BABYLONVIDEOTEXTURE);
+
+  // for custom mesh see https://babylonjsguide.github.io/advanced/Custom
+  const videoMesh=new BABYLON.Mesh("custom", BABYLONSCENE);
+  videoMesh.alwaysSelectAsActiveMesh = true; // disable frustum culling
+  const vertexData = new BABYLON.VertexData();
+  vertexData.positions = [-1,-1,1,   1,-1,1,   1,1,1,   -1,1,1]; // z is set to 1 (zfar)
+  vertexData.indices = [0,1,2, 0,2,3];  
+  vertexData.applyToMesh(videoMesh);
+  videoMesh.material=videoMaterial;
+  
   // CREATE THE CAMERA:
-  const aspecRatio = spec.canvasElement.width / spec.canvasElement.height;
-  THREECAMERA = new THREE.PerspectiveCamera(SETTINGS.cameraFOV, aspecRatio, 0.1, 100);
+  BABYLONCAMERA = new BABYLON.Camera('camera', new BABYLON.Vector3(0,0,0), BABYLONSCENE);
+  BABYLONSCENE.setActiveCameraByName('camera');
+  BABYLONCAMERA.fov = SETTINGS.cameraFOV * Math.PI/180;
+  BABYLONCAMERA.minZ = 0.1;
+  BABYLONCAMERA.maxZ = 100;
+  ASPECTRATIO = BABYLONENGINE.getAspectRatio(BABYLONCAMERA);
+} //end init_babylonScene()
 
-  // replace the default Aframe camera by our camera:
-  AFRAMEINSTANCE.camera = THREECAMERA;
-
-  ISLOADED = true;
-} //end init_threeScene()
-
-//launched by body.onload() :
+// entry point:
 function main(){
   JEEFACEFILTERAPI.init({
     canvasId: 'jeeFaceFilterCanvas',
@@ -135,51 +117,54 @@ function main(){
         return;
       }
 
-      console.log('INFO: JEEFACEFILTERAPI IS READY');
-      init_aFrame(spec);
+      console.log('INFO : JEEFACEFILTERAPI IS READY');
+      init_babylonScene(spec);
     }, //end callbackReady()
 
     // called at each render iteration (drawing loop):
     callbackTrack: function(detectState){
-      if (!ISLOADED){
-        return;
-      }
-
       if (ISDETECTED && detectState.detected<SETTINGS.detectionThreshold-SETTINGS.detectionHysteresis){
-        //DETECTION LOST
+        // DETECTION LOST
         detect_callback(false);
         ISDETECTED = false;
       } else if (!ISDETECTED && detectState.detected>SETTINGS.detectionThreshold+SETTINGS.detectionHysteresis){
-        //FACE DETECTED
+        // FACE DETECTED
         detect_callback(true);
         ISDETECTED = true;
       }
 
       if (ISDETECTED){
-        // move the cube in order to fit the head
-        const tanFOV = Math.tan(THREECAMERA.aspect*THREECAMERA.fov*Math.PI/360); //tan(FOV/2), in radians
-        const W = detectState.s;  //relative width of the detection window (1-> whole width of the detection window)
-        const D = 1 / (2*W*tanFOV); //distance between the front face of the cube and the camera
+        // move the cube in order to fit the head:
+        const tanFOV = Math.tan(ASPECTRATIO*BABYLONCAMERA.fov/2); // tan(FOV/2), in radians
+        const W = detectState.s;  // relative width of the detection window (1-> whole width of the detection window)
+        const D = 1 / (2*W*tanFOV); // distance between the front face of the cube and the camera
         
         // coords in 2D of the center of the detection window in the viewport:
         const xv = detectState.x;
         const yv = detectState.y;
         
         // coords in 3D of the center of the cube (in the view coordinates system):
-        const z = -D-0.5;   // minus because view coordinate system Z goes backward. -0.5 because z is the coord of the center of the cube (not the front face)
-        const x = xv * D * tanFOV;
-        const y = yv * D * tanFOV / THREECAMERA.aspect;
+        var z=-D-0.5;   // minus because view coordinate system Z goes backward. -0.5 because z is the coord of the center of the cube (not the front face)
+        var x=xv*D*tanFOV;
+        var y=yv*D*tanFOV/ASPECTRATIO;
 
-        //move and rotate the cube:
-        THREEFACEOBJ3D.position.set(x,y+SETTINGS.pivotOffsetYZ[0],z+SETTINGS.pivotOffsetYZ[1]);
-        THREEFACEOBJ3D.rotation.set(detectState.rx+SETTINGS.rotationOffsetX, detectState.ry, detectState.rz, "XYZ");
+        // move and rotate the cube:
+        BABYLONFACEOBJ3D.position.set(x,y+SETTINGS.pivotOffsetYZ[0],-z-SETTINGS.pivotOffsetYZ[1]);
+        BABYLONFACEOBJ3D.rotation.set(-detectState.rx+SETTINGS.rotationOffsetX, -detectState.ry, detectState.rz);//"XYZ" rotation order;
+      
+        // mouth opening:
+        let mouthOpening = detectState.expressions[0];
+        mouthOpening = smoothStep(0.35, 0.7, mouthOpening);
+        JAWMESH.position.y = -(0.5+0.15+0.01+0.7*mouthOpening*0.5);
       }
 
-      // reinitialize the state of THREE.JS because JEEFACEFILTER have changed stuffs:
-      THREERENDERER.state.reset();
-
-      // trigger the render of the THREE.JS SCENE:
-      THREERENDERER.render(THREESCENE, THREECAMERA);
+      // reinitialize the state of BABYLON.JS because JEEFACEFILTER have changed stuffs:
+      BABYLONENGINE.wipeCaches(true);
+      
+      // trigger the render of the BABYLON.JS SCENE:
+      BABYLONSCENE.render();
+      
+      BABYLONENGINE.wipeCaches();
     } //end callbackTrack()
   }); //end JEEFACEFILTERAPI.init call
 } //end main()
